eb63d638ee0dec4bc19010c8ed28396141e42519
Bruce Graham <brucegraham11@gmail.com>
2017-09-15 22:59:06 -0700

    Add node/express server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const secret = require('./config.js');
+const dbHelpers = require('./dbhelpers.js');
+const apiHelpers = require('./apihelpers.js');
+const User = require('./database.js');
+
+
+app.post('/api/testUser', function(err, response) {
+  if (err) {
+    console.log('/api/testUser error', err);
+  }
+  dbHelpers.createUser();
+  response.send('went through');
+});
+
+app.get('/api/testMovie', function(err, response) {
+  if (err) {
+    console.log('/api/testMovie error', err);
+    apiHelpers.getMovie();
+    response.send('perhaps we got the movie');
+  }
+})
+
+app.listen('3000', () => {
+  console.log('Server connected on port 3000');
+});
