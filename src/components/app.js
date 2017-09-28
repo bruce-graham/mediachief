46a5af7ed401f421ba73193359c8ec72aadd0357
Bruce <brucegraham11@gmail.com>
2017-09-28 10:48:42 -0700

    Add React/Redux basic starting file structure

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  };
+
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    );
+  }
+};
+
+export default App;
