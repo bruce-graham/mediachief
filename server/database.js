d685985b398bcc762aff7eb6c884e64e00e5f31f
Bruce <brucegraham11@gmail.com>
2017-09-28 10:51:42 -0700

    Clean up of syntax

--- server/database.js
+++ server/database.js
@@ -12,11 +12,9 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 db.on('error', console.error.bind(console, 'db connection error'));
-db.once('open', function () {
 
-
+db.once('open', () => {
   console.log('Connected to the Database');
-
 });
 
 module.exports = User;
